3637a86fe3e20ed04e9904bbccec38249ebb9603
DL030 <mihirsane4@gmail.com>
2019-06-25 17:45:16 +0530

    registartion complete

--- api/user.php
+++ api/user.php
@@ -10,6 +10,11 @@
     
    require_once('dbConnect.php');
 
+
+   if ($conn->connect_error) {
+        return $response->withJson(array('message' =>"unsucessfull" ),500, JSON_PRETTY_PRINT);      
+   }
+
    $data=array();
    
    $sql = "SELECT * FROM `users` ORDER BY id";
@@ -20,66 +25,68 @@
    	  $data[]=$row;
     }
 
-    echo json_encode(array("User"=>$data) , JSON_PRETTY_PRINT);   	
-    
     mysqli_close($conn);
-   
+
+    return $response->withJson(array('message' =>"sucessfull",
+                                      "User"=>$data ),200,   JSON_PRETTY_PRINT);
+    
    });
 
 
-   //Get sepcific user
-   //url is "http://localhost/Projects/slim_api_fremwork/v1/users/2"
-   //in retrofit use @path you can increse parameter ........ or multiple ex. /users/{id}/{}
-   $app->get('/users/{id}', function ($request) {
-    
-    require_once('dbConnect.php');
-
-    $id =$request-> getAttribute('id');
-
-    $sql = "SELECT * FROM `users` WHERE id=$id";
-  
-    $result = $conn->query($sql);
-
-   	$data=$result->fetch_assoc();
    
-    if(isset($data)){
-      echo json_encode(array("User"=>$data), JSON_PRETTY_PRINT);   	
-    }
-
-    mysqli_close($conn);
-
-   });
-
-  
     //Post data and create a new record
     //if you use post than body is form encoded
     //url is "http://localhost/Projects/slim_api_fremwork/v1/users/insert"
     //in retrofit use POST with @field or @body.....
-    $app->post('/users/insert', function ($request,$response) {
+    $app->post('/users/register', function ($request,$response) {
     
       require_once('dbConnect.php');
 
-      $name = $request->getParsedBody()['name'];
-      $username = $request->getParsedBody()['username'];
+      if ($conn->connect_error) {
+        return $response->withJson(array('message' =>"Unsucessfull" ),500, JSON_PRETTY_PRINT);      
+      }
+
+      $first_name = $request->getParsedBody()['first_name'];
+      $last_name  = $request->getParsedBody()['last_name'];
+      $email	    = $request->getParsedBody()['email'];
+      $mobile_no = $request->getParsedBody()['mobile_no'];
       $password = $request->getParsedBody()['password'];
-      $api_key = $request->getParsedBody()['api_key'];
+      $created_at = time();
       
+      if(isset($first_name) && 
+         isset($last_name) &&
+         isset($email) &&
+         isset($mobile_no) &&
+         isset($password))
 
-   $sql="INSERT INTO users(name,username,password,api_key) VALUES (
-                              '$name','$username','$password','$api_key')";
-  
-      $insert=mysqli_query($conn,$sql);
-  
-        if($insert){
-    
-          echo json_encode(array('response' =>"sucessfull" ));
-        }
-        else{
-          echo json_encode(array('response' =>"Unsucessfull" ));
-        }
-         mysqli_close($conn, JSON_PRETTY_PRINT);
+      {   
+        $_sql = "SELECT * FROM `users` WHERE email='$email'";  
+        $result = mysqli_query($conn, $_sql);
 
+        if(mysqli_num_rows($result) == 0){
+          
+               $sql="INSERT INTO users(first_name,last_name,email,mobile_no,password,created_at) VALUES (
+                                    '$first_name','$last_name','$email','$mobile_no','$password','$created_at')";
+        
+                $insert=mysqli_query($conn,$sql);
+                mysqli_close($conn);
+
+                if($insert){
+                  return $response->withJson(array('message' =>"sucessfull" ),201,   JSON_PRETTY_PRINT);
+                }
+                else{
+                  return $response->withJson(array('message' =>"Unsucessfull"),400 , JSON_PRETTY_PRINT);
+                }
+          }else{
+               return $response->withJson(array('message' =>"Unsucessfull"),409 , JSON_PRETTY_PRINT);
+          }
+      }
+      else{
+          return $response->withJson(array('message' =>"Unsucessfull" ),400, JSON_PRETTY_PRINT);
+      }
    });
+
+ 
 
 
 	//PUT data and update a record
@@ -117,7 +124,7 @@
     //if you use post than body in row and text in json
     //url is "http://localhost/Projects/slim_api_fremwork/v1/users/body"
     //in retrofit use POST with @body...
-    $app->post('/users/body', function ($request,$response) {
+    $app->post('/users/imap_body(imap_stream, msg_number)', function ($request,$response) {
     
       require_once('dbConnect.php');
 
@@ -147,3 +154,27 @@
 
 
 		
+//Get sepcific user
+   //url is "http://localhost/Projects/slim_api_fremwork/v1/users/2"
+   //in retrofit use @path you can increse parameter ........ or multiple ex. /users/{id}/{}
+    $app->get('/users/{id}', function ($request) {
+    
+      require_once('dbConnect.php');
+  
+      $id =$request-> getAttribute('id');
+  
+      $sql = "SELECT * FROM `users` WHERE id=$id";
+    
+      $result = $conn->query($sql);
+  
+       $data=$result->fetch_assoc();
+     
+      if(isset($data)){
+        echo json_encode(array("User"=>$data), JSON_PRETTY_PRINT);   	
+      }
+  
+      mysqli_close($conn);
+  
+     });
+  
+    
